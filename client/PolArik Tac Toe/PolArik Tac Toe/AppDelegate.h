23b5251f19d5104bb195b53316c59ef8eba75796
Frederik Riedel <f@polarapps.de>
2014-12-25 14:44:45 +0100

    added Xcode Project for Client

---
+++ client/PolArik Tac Toe/PolArik Tac Toe/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  PolArik Tac Toe
+//
+//  Created by Frederik Riedel on 25.12.14.
+//  Copyright (c) 2014 Frederik Riedel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MainViewController.h"
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ client/PolArik Tac Toe/PolArik Tac Toe/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PolArik Tac Toe
+//
+//  Created by Frederik Riedel on 25.12.14.
+//  Copyright (c) 2014 Frederik Riedel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
